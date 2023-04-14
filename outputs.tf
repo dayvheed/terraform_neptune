5c073cc3add13179a6b4635a3fb3aa720ab73a49
David Obembe <dayvheed@Davids-MacBook-Air.local>
2023-04-14 09:10:07 +0300

    added the arn as output

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,7 @@ output "ec2_instance_dns" {
 output "ec2_public_ip" {
   value = aws_instance.neptune-ec2-connector.public_ip
 }
+
+output "role_arn" {
+  value = [aws_iam_role.neptune_loader_role.arn]
+}
